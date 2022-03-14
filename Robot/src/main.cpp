3191e804c7316ad4b1eee081bb3896a2347d0fe3
GogoVega <ga.dandele@students.ephec.be>
2022-03-14 12:18:31 +0100

    first elements

--- Robot/src/main.cpp
+++ Robot/src/main.cpp
@@ -1,9 +1,125 @@
+// MIT License
+
+// Copyright (c) 2022 Gauthier Dandele
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <SerialTransfer.h>
+#include <Wire.h>
+#include <SPI.h>
+
+SerialTransfer RecTransfer;
+
+const int borneENA = A0;       // Vitesse Motor A
+const int borneIN1 = 20;       // Marche avant
+const int borneIN2 = 21;       // Marche arrière
+const int borneIN3 = 22;       // Marche avant
+const int borneIN4 = 23;       // Marche arrière
+const int borneENB = A1;       // Vitesse Motor B
+const int borneENC = A2;       // Vitesse Motor C
+const int borneIN5 = 24;       // Marche avant
+const int borneIN6 = 25;       // Marche arrière
+const int borneIN7 = 26;       // Marche avant
+const int borneIN8 = 27;       // Marche arrière
+const int borneEND = A3;       // Vitesse Motor D
+
+// LCD
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+struct STRUCT {
+  int AxeX_Robot;
+  int AxeY_Robot;
+  int BP_OP;
+  int BP_UD;
+  int RFID_State;
+  int Distance;
+} data;
+
+void WriteVitesseRobot(int v1, int v2) {
+  if (v1 >= 50) {
+    digitalWrite(borneIN1, HIGH);
+    digitalWrite(borneIN2, LOW);
+    analogWrite(borneENA, v1);
+  } else if (v1 <= -50) {
+    digitalWrite(borneIN1, LOW);
+    digitalWrite(borneIN2, HIGH);
+    analogWrite(borneENA, abs(v1));
+  } else {
+    digitalWrite(borneIN1, LOW);
+    digitalWrite(borneIN2, LOW);
+    analogWrite(borneENA, 0);
+  }
+  if (v2 >= 50) {
+    digitalWrite(borneIN3, HIGH);
+    digitalWrite(borneIN4, LOW);
+    analogWrite(borneENB, v2);
+  } else if (v2 <= -50) {
+    digitalWrite(borneIN3, LOW);
+    digitalWrite(borneIN3, HIGH);
+    analogWrite(borneENB, abs(v2));
+  } else {
+    digitalWrite(borneIN3, LOW);
+    digitalWrite(borneIN4, LOW);
+    analogWrite(borneENB, 0);
+  }
+}
+
+void ReadVitesseRobot(){
+  int VRX = data.AxeX_Robot;
+  int VRY = data.AxeY_Robot;
+  int vitesseG, vitesseD;
+
+  // Gauche - Doite
+  if (VRY > 100){
+    vitesseG = max(VRX - 50, -255);
+    vitesseD = min(VRX + 50, 255);
+  } else if (VRY < -100) {
+    vitesseG = min(VRX + 50, 255);
+    vitesseD = max(VRX - 50, -255);
+  } else {
+    vitesseG = VRX;
+    vitesseD = VRX;
+  }
+  WriteVitesseRobot(vitesseG, vitesseD);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial1.begin(38400);
+  RecTransfer.begin(Serial1);
+
+  // LCD
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("DEMARRAGE");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint16_t recSize = 0;
+  recSize = RecTransfer.txObj(data, recSize);
+  RecTransfer.sendData(recSize);
+
+  if(RecTransfer.available()) {
+    uint16_t recSize = 0;
+    recSize = RecTransfer.rxObj(data, recSize);
+  }
 }
--- Telecommande/src/main.cpp
+++ Telecommande/src/main.cpp
@@ -1,9 +1,137 @@
+// MIT License
+
+// Copyright (c) 2022 Gauthier Dandele
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <SerialTransfer.h>
+#include <Wire.h>
+
+SerialTransfer SendTransfer;
+
+// LCD
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Joysticks
+const int AxeX_Gauche = A0, AxeY_Gauche = A1;   // Robot
+const int AxeX_Droite = A2, AxeY_Droite = A3;   // Bras
+
+// Bouton Poussoir Pince
+const int OpenPince = 8, ClosePince = 9;        // Ouvrir-Fermer Pince
+const int UpPince = 10, DownPince = 11;         // Monter-Descendre Pince
+
+// Batterie
+const int PinBattery = A4;
+
+// Structure des données
+struct STRUCT {
+  int AxeX_Robot;
+  int AxeY_Robot;
+  int BP_OP;
+  int BP_UD;
+  int RFID_State;
+  int Distance;
+} data;
+
+// Mapping value for -255 to 255
+int JoystickValue(int pin) {
+  int value = map(analogRead(pin), 0, 1023, -255, 255);
+  return value;
+}
+
+// CMD Ouverture-Fermeture de la Pince
+int EtatBP_OC() {
+  int BP1 = digitalRead(OpenPince);
+  int BP2 = digitalRead(ClosePince);
+  int Read = data.BP_OP;
+  int Write;
+
+  // Open - Close
+  if(BP1 && !Read) {
+    Write = 1;
+  } else if (BP2 && Read) {
+    Write = 0;
+  }
+  return Write;
+}
+
+// CMD Moter-Descendre la pince
+int EtatBP_UD() {
+  int BP3 = digitalRead(UpPince);
+  int BP4 = digitalRead(DownPince);
+  int Write;
+
+  // Arrêt - Monter - Descendre
+  if(BP3 && !BP4) {
+    Write = 1;
+  } else if (BP4 && !BP3) {
+    Write = 2;
+  } else {
+    Write = 0;
+  }
+  return Write;
+}
+
+// Valeur Batterie
+float Batterie() {
+  float ChargeLevel, Tension;
+  Tension = analogRead(PinBattery)*0.85;
+  ChargeLevel = map(Tension, 875, 750, 100, 0);
+  return ChargeLevel;
+}
+
+// Gestion du LCD
+void Display(String a, int x, int y) {
+  lcd.clear();
+  lcd.setCursor(x,y);
+  lcd.print(a);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial1.begin(38400);
+  SendTransfer.begin(Serial1);
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("DEMARRAGE");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint16_t sendSize = 0;
+  data.AxeX_Robot = JoystickValue(AxeX_Gauche);
+  data.AxeY_Robot = JoystickValue(AxeY_Gauche);
+  data.BP_OP = EtatBP_OC();
+  data.BP_UD = EtatBP_UD();
+  sendSize = SendTransfer.txObj(data, sendSize);
+  SendTransfer.sendData(sendSize);
+
+  // Si message reçu => Lecture
+  if (SendTransfer.available()) {
+    uint16_t sendSize = 0;
+    sendSize = SendTransfer.rxObj(data, sendSize); 
+  }
+
+  Display("PRET",0,0);
+
+  delay(1000);
 }
