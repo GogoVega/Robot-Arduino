06612163c40ccf9eda37fff5f6967c2a8c29a1c8
Skn02 <64737065+Skn02@users.noreply.github.com>
2022-06-01 23:27:07 +0200

    Update and rename Telecommande/include/Lcd_Display.h to Robot/include/Lcd_Display.h

---
+++ Robot/include/Lcd_Display.h
@@ -0,0 +1,34 @@
+String LCDState() {
+  if (!digitalRead(BluethoothPin)) {
+    return "  Connexion...";
+  } else {
+    switch (data.RFID_State) {
+      case 0:
+        return "   Verrouille";
+      case 1:
+        return " Deverrouille";
+      case 2:
+        return "Badgez nvll carte";    // Affichage des messages sur Lcd après connexion Blueth
+      case 3:
+        return "Carte ajoutee";
+      case 4:
+        return "Carte refusee!";
+      case 5:
+        return "Aucune carte!";
+    }
+  }
+
+  return "ERROR";
+}
+
+
+// Valeurs Batterie                     
+float* Batterie() {
+  static float Output[2] = {};
+  const float Tension = map(analogRead(BatteryPin), 0, 1023, 0, 500);
+  const float ChargeLevel = map(Tension, 0, 500, 0, 10000);                              //Conversion des entrées analogiques , valeur tension et baterrie
+   Output[0] = Tension / 100;
+  Output[1] = ChargeLevel / 100;
+
+  return Output;
+}
