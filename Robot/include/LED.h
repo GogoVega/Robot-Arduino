01a4314f1a18097d1f443e6bf8a893221848dd88
RIFINHO <79934363+RIFINHO@users.noreply.github.com>
2022-06-09 21:59:59 +0200

    Update LED.h

--- Robot/include/LED.h
+++ Robot/include/LED.h
@@ -1,3 +1,26 @@
+```c
+#include <Adafruit_NeoPixel.h>
+#include <type.h>
+
+Adafruit_NeoPixel strip(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+// Alerte lumineuse
+void Alert(uint32_t color) {
+  for(int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, color);
+  }
+
+  // 250ms
+  if (Flag == 1 || Flag == 11) {
+    strip.show();
+  } else if (Flag == 6 || Flag == 16) {
+    strip.clear();
+  }
+}
+
+// Clignotement
+int Blinking(uint32_t color, int num, int actual) {
+  static int sens = 1;
 
   // INIT
   if (actual == 0) {
