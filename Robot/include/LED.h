b645409d4f7acbd310d200c09018a10e8e103f10
RIFINHO <79934363+RIFINHO@users.noreply.github.com>
2022-06-09 21:52:09 +0200

    Update LED.h

--- Robot/include/LED.h
+++ Robot/include/LED.h
@@ -26,3 +26,18 @@ int Blinking(uint32_t color, int num, int actual) {
   if (actual == 0) {
     sens = 1;
   }
+  
+   if (sens == 1) {
+    if (actual < num) {
+      strip.setPixelColor(actual, color);
+      strip.show();
+      actual++;
+    } else {
+      sens = -1;
+    }
+  } if (sens == -1) {
+    if ((actual - 1) >= 0) {
+      strip.setPixelColor(actual, strip.Color(0, 0, 0));
+      strip.show();
+      actual--;
+ 
