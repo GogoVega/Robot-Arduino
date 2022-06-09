a719e794164f7caf115fcdc234a6a967ac557516
RIFINHO <79934363+RIFINHO@users.noreply.github.com>
2022-06-09 21:50:44 +0200

    Create LED.h

---
+++ Robot/include/LED.h
@@ -0,0 +1,20 @@
+```c
+#include <Adafruit_NeoPixel.h>
+#include <type.h>
+
+Adafruit_NeoPixel strip(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+// Alerte lumineuse
+void Alert(uint32_t color) {
+  for(int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, color);
+  }
+
+  // 250ms
+  if (Flag == 1 || Flag == 11) {
+    strip.show();
+  } else if (Flag == 6 || Flag == 16) {
+    strip.clear();
+  }
+}
+
