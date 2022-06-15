af7bf0e0f3f682b8835813912c9a7beaebdeaf09
RIFINHO <79934363+RIFINHO@users.noreply.github.com>
2022-06-15 13:08:11 +0200

    Update serveomoteur.h

--- Robot/include/serveomoteur.h
+++ Robot/include/serveomoteur.h
@@ -1,1 +1,23 @@
-a
+/* Inclut la librairie Servo pour manipuler le servomoteur */
+#include <Servo.h>
+
+int bp1 = 2;
+int bp2 = 7;
+
+int position = 0;
+
+/* Créer un objet Servo pour contrôler le servomoteur */
+Servo monServomoteur;
+
+void setup()
+{
+  // Bouton poussoir 1
+  pinMode(bp1, INPUT_PULLUP);
+  pinMode(bp2, INPUT_PULLUP);
+
+  // Attache le servomoteur à la broche 2
+  monServomoteur.attach(5);
+  monServomoteur.write(0);
+
+  Serial.begin(9600);
+}
