bce6bdb949478c861593ba556988d61a3e7666d6
RIFINHO <79934363+RIFINHO@users.noreply.github.com>
2022-06-15 13:09:43 +0200

    Update serveomoteur.h

--- Robot/include/serveomoteur.h
+++ Robot/include/serveomoteur.h
@@ -21,3 +21,28 @@ void setup()
 
   Serial.begin(9600);
 }
+void loop()
+{
+  // Récuperer valeur de bp1
+  int b1 = digitalRead(bp1);
+  int b2 = digitalRead(bp2);
+
+  // Si Appuie BP1
+  if (b1 == LOW) {
+    monServomoteur.write(position);
+    position = position + 1;
+    if (position > 180) {
+      position = 180;
+    }
+    delay(10);
+  }
+
+  // Si Appuie BP2
+  if (b2 == LOW) {
+    monServomoteur.write(position);
+    position = position - 1;
+    if (position < 0) {
+      position = 0;
+    }
+    delay(10);
+  }
