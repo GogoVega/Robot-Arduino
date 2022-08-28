f876ff8d0491d722e4efaffdf2b4cca337492542
EL KADI <79934363+RIFINHO@users.noreply.github.com>
2022-08-28 18:10:59 +0200

    Update lcd2.h

--- Robot/include/lcd2.h
+++ Robot/include/lcd2.h
@@ -5,7 +5,14 @@
 #define control_input A0
 #define max_distance 200
 
-const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7, trig = 8, echo = 9;
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+int ledRed = 8, ledGreen = 9 ;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-NewPing sonar(trigger_output, echo_input, max_distance);
-int rawvalue = 0;
+const int resistanceP = A0;
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  // Print a message to the LCD.
+}
